bd2a6f19aab146704046a21087cd439bdb584c09
jonasvrnnn <jonas.verbruggen@student.ap.be>
2025-02-28 14:26:54 +0100

    MileStone1

--- script.ts
+++ script.ts
@@ -1,3 +1,33 @@
 import heroes from "./folder1.json";
+import * as readline from "readline-sync";
 
-console.log(heroes);
+let keuze = "";
+do {
+  console.log(
+    "Welcome to de JSON dat viewer\n\n1. View all data\n2. Filter by ID\n3. Exit\n\n"
+  );
+  keuze = readline.question("Please enter your choice: ");
+  switch (keuze) {
+    case "1":
+      heroes.forEach((e) => {
+        console.log(`${e.heroName} (${e.id})`);
+      });
+      readline.question("\nDruk op Enter om verder te gaan...");
+      break;
+    case "2":
+      const id = readline.question(
+        "Please enter the ID you want to filter by: "
+      );
+      console.log(
+        heroes.filter((e) => {
+          return e.id === Number(id);
+        })
+      );
+      readline.question("\nDruk op Enter om verder te gaan...");
+      break;
+    case "3":
+      break;
+    default:
+      break;
+  }
+} while (keuze !== "3");
